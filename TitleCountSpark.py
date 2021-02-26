9f7a7c64603db887d292a5559dfd1a9748920aa9
Michael Tallman <mtallman@codemiser.com>
2021-02-26 18:59:52 +0000

    Part A complete

--- TitleCountSpark.py
+++ TitleCountSpark.py
@@ -7,12 +7,25 @@ from pyspark import SparkConf, SparkContext
 
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
-
+stopWords = []
+delims = []
 with open(stopWordsPath) as f:
 	#TODO
+    stopWords  = f.read().splitlines()
 
 with open(delimitersPath) as f:
     #TODO
+    delims= list(f.read())
+
+def splitLine(line):
+    goodWords = []
+    for delim in delims:
+        line = line.replace(delim," ")
+    words = line.split()
+    for word in words:
+        if word.lower() not in stopWords:
+            goodWords.append((word.lower(), 1))
+    return goodWords
 
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
@@ -21,8 +34,13 @@ sc = SparkContext(conf = conf)
 lines = sc.textFile(sys.argv[3],1)
 
 #TODO
-
-outputFile = open(sys.argv[4],"w")
+mapLines = lines.flatMap(splitLine)
+counts = mapLines.reduceByKey( lambda a, b: a + b)
+topLines = counts.sortBy(lambda a: a[1], ascending=False).take(10)
+outputLines = sorted(topLines)
+outputFile = open(sys.argv[4],"w", errors='ignore')
+for aLine in outputLines:
+    outputFile.write("{}\t{}\n".format(aLine[0], aLine[1]))
 
 #TODO
 #write results to output file. Foramt for each line: (line +"\n")
